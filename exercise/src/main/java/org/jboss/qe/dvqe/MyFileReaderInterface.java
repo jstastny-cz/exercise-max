7299187b85d00024def214efeec063a681c779f5
mkosiarc <mkosiarc@dhcp-10-40-4-108.brq.redhat.com>
2017-11-08 16:01:59 +0100

    update

--- exercise/src/main/java/org/jboss/qe/dvqe/MyFileReaderInterface.java
+++ exercise/src/main/java/org/jboss/qe/dvqe/MyFileReaderInterface.java
@@ -1,29 +1,35 @@
 package org.jboss.qe.dvqe;
 
 import java.util.List;
+
 /**
-* MyFileReader can read a text file from given location 
-*
-**/
-public interface MyFileReaderInterface{
-/** 
-* Method getNumberOfLines returns the number of rows this file has.
-*
-**/
-int getNumberOfLines();
-/**
-* Method getNumberOfNonEmptyLines returns number of non-empty rows from the file.
-**/
-int getNumberOfNonEmptyLines();
-/**
-* Method readLines reads contents of the file an returns lines read.
-* @return the lines read
-**/
-List<String> readLines();
-/**
-* Method readFirstNLines reads first n lines from the file.
-* @return the lines read.
-**/
-List<String> readFirstNLines(int numOfLines);
+ * MyFileReader can read a text file from given location
+ *
+ **/
+public interface MyFileReaderInterface {
+    /**
+     * Method getNumberOfLines returns the number of rows this file has.
+     *
+     **/
+    int getNumberOfLines();
+
+    /**
+     * Method getNumberOfNonEmptyLines returns number of non-empty rows from the file.
+     **/
+    int getNumberOfNonEmptyLines();
+
+    /**
+     * Method readLines reads contents of the file an returns lines read.
+     * 
+     * @return the lines read
+     **/
+    List<String> readLines();
+
+    /**
+     * Method readFirstNLines reads first n lines from the file.
+     * 
+     * @return the lines read.
+     **/
+    List<String> readFirstNLines(int numOfLines);
 
 }
--- exercise/src/test/java/org/jboss/qe/dvqe/MyFileReaderTest.java
+++ exercise/src/test/java/org/jboss/qe/dvqe/MyFileReaderTest.java
@@ -7,60 +7,63 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-
 @Test
 public class MyFileReaderTest {
-  
+
     MyFileReaderUsingUtils fr;
     File file;
-  
+
     @BeforeClass
-    public void setUp() {
+    public void setUp() throws FileNotFoundException {
         ClassLoader classLoader = getClass().getClassLoader();
         file = new File(classLoader.getResource("testfile.txt").getPath());
         fr = new MyFileReaderUsingUtils(file.getPath());
     }
-	
+
     @Test
     public void getNumberOfLinesTest() {
-	    assertEquals(fr.getNumberOfLines(),8,"Unexpected number of lines");
-	    assertNotEquals(fr.getNumberOfLines(),0,"Unexpected number of lines");
-        assertNotEquals(fr.getNumberOfLines(),-1,"Unexpected number of lines");
-        assertNotEquals(fr.getNumberOfLines(),321,"Unexpected number of lines");
+        assertEquals(fr.getNumberOfLines(), 8, "Unexpected number of lines");
+        assertNotEquals(fr.getNumberOfLines(), 0, "Unexpected number of lines");
+        assertNotEquals(fr.getNumberOfLines(), -1, "Unexpected number of lines");
+        assertNotEquals(fr.getNumberOfLines(), 321, "Unexpected number of lines");
     }
+
     @Test
     public void getNumberOfNonEmptyLinesTest() throws IOException {
-        assertEquals(fr.getNumberOfNonEmptyLines(),4,"Unexpected number of non-empty lines");
-        assertNotEquals(fr.getNumberOfNonEmptyLines(),0,"Unexpected number of lines");
-        assertNotEquals(fr.getNumberOfNonEmptyLines(),-1,"Unexpected number of lines");
-        assertNotEquals(fr.getNumberOfNonEmptyLines(),321,"Unexpected number of lines");
+        assertEquals(fr.getNumberOfNonEmptyLines(), 4, "Unexpected number of non-empty lines");
+        assertNotEquals(fr.getNumberOfNonEmptyLines(), 0, "Unexpected number of lines");
+        assertNotEquals(fr.getNumberOfNonEmptyLines(), -1, "Unexpected number of lines");
+        assertNotEquals(fr.getNumberOfNonEmptyLines(), 321, "Unexpected number of lines");
     }
+
     @Test
-    public void readFirstNLinesTest() {  
-        List<String> testLines = Arrays.asList(new String[]{"first","second"});
+    public void readFirstNLinesTest() {
+        List<String> testLines = Arrays.asList(new String[] { "first", "second" });
         List<String> actualLines = fr.readFirstNLines(2);
-        assertEquals(actualLines.size(),2,"Unexpected size of list");
-        assertNotEquals(actualLines.size(),0,"Unexpected size of list");
-        assertNotEquals(actualLines.size(),-1,"Unexpected size of list");
-        assertNotEquals(actualLines.size(),321,"Unexpected size of list");
-	    for(int i =0;i < actualLines.size();i++) {
-		    assertEquals(actualLines.get(i),testLines.get(i),"Unexpected element value");
-	    }
+        assertEquals(actualLines.size(), 2, "Unexpected size of list");
+        assertNotEquals(actualLines.size(), 0, "Unexpected size of list");
+        assertNotEquals(actualLines.size(), -1, "Unexpected size of list");
+        assertNotEquals(actualLines.size(), 321, "Unexpected size of list");
+        for (int i = 0; i < actualLines.size(); i++) {
+            assertEquals(actualLines.get(i), testLines.get(i), "Unexpected element value");
+        }
     }
+
     @Test
     public void readLinesTest() {
-        List<String> testLines = Arrays.asList(new String[]{"first","second","","fourth","","","seventh",""});
+        List<String> testLines = Arrays.asList(new String[] { "first", "second", "", "fourth", "", "", "seventh", "" });
         List<String> actualLines = fr.readLines();
-        assertEquals(actualLines.size(),8,"Unexpected size of list");
-        assertNotEquals(actualLines.size(),0,"Unexpected size of list");
-        assertNotEquals(actualLines.size(),-1,"Unexpected size of list");
-        assertNotEquals(actualLines.size(),321,"Unexpected size of list");
-        for(int i =0;i < actualLines.size();i++) {
-		    assertEquals(actualLines.get(i),testLines.get(i),"Unexpected element value");
-	    }
+        assertEquals(actualLines.size(), 8, "Unexpected size of list");
+        assertNotEquals(actualLines.size(), 0, "Unexpected size of list");
+        assertNotEquals(actualLines.size(), -1, "Unexpected size of list");
+        assertNotEquals(actualLines.size(), 321, "Unexpected size of list");
+        for (int i = 0; i < actualLines.size(); i++) {
+            assertEquals(actualLines.get(i), testLines.get(i), "Unexpected element value");
+        }
     }
 }
