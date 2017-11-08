b23a0fd894f2dc83101c602dc49a0dc888552d1f
mkosiarc <mkosiarc@dhcp-10-40-4-108.brq.redhat.com>
2017-11-08 16:57:38 +0100

    updated javadoc, change return values on failure

--- exercise/src/main/java/org/jboss/qe/dvqe/MyFileReader.java
+++ exercise/src/main/java/org/jboss/qe/dvqe/MyFileReader.java
@@ -2,8 +2,8 @@ package org.jboss.qe.dvqe;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,43 +12,47 @@ class MyFileReader implements MyFileReaderInterface {
     File file = null;
     Scanner scanner = null;
 
-    MyFileReader(String path) {
-        try {
-            file = new File(path);
-        } catch (NullPointerException e) {
-            System.err.println("Missing path");
+    MyFileReader(String path) throws FileNotFoundException {
+        if (path == null || path.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        file = new File(path);
+        if (!file.isFile()) {
+            throw new FileNotFoundException();
         }
     }
 
-    public Scanner getScanner() {
-        try {
-            scanner = new Scanner(new BufferedReader(new FileReader(file)));
-        } catch (IOException e) {
-            System.err.println("Error loading file");
-        }
+    public Scanner getScanner() throws FileNotFoundException {
+        scanner = new Scanner(new BufferedReader(new FileReader(file)));
         return scanner;
     }
 
     public int getNumberOfLines() {
-        int numOfLines = 0;
+        int numOfLines = -1;
         try (Scanner sc = getScanner()) {
+            numOfLines = 0;
             while (sc.hasNextLine()) {
                 numOfLines++;
                 sc.nextLine();
             }
-            return numOfLines;
+        } catch (FileNotFoundException e) {
+            System.err.println("File not Found");
         }
+        return numOfLines;
     }
 
     public int getNumberOfNonEmptyLines() {
-        int numOfNonEmptyLines = 0;
+        int numOfNonEmptyLines = -1;
         try (Scanner scanner = getScanner()) {
+            numOfNonEmptyLines = 0;
             while (scanner.hasNextLine()) {
                 if (!(scanner.nextLine().isEmpty()))
                     numOfNonEmptyLines++;
             }
-            return numOfNonEmptyLines;
+        } catch (FileNotFoundException e) {
+            System.err.println("File not Found");
         }
+        return numOfNonEmptyLines;
     }
 
     public List<String> readLines() {
@@ -58,8 +62,11 @@ class MyFileReader implements MyFileReaderInterface {
             while (scanner.hasNextLine()) {
                 lines.add(scanner.nextLine());
             }
-            return lines;
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not Found");
         }
+        return lines;
     }
 
     public List<String> readFirstNLines(int numOfLines) {
@@ -68,6 +75,8 @@ class MyFileReader implements MyFileReaderInterface {
             lines = new ArrayList<String>();
             for (int i = 0; scanner.hasNextLine() && i < numOfLines; i++)
                 lines.add(scanner.nextLine());
+        } catch (FileNotFoundException e) {
+            System.err.println("File not Found");
         }
         return lines;
     }
--- exercise/src/main/java/org/jboss/qe/dvqe/MyFileReaderInterface.java
+++ exercise/src/main/java/org/jboss/qe/dvqe/MyFileReaderInterface.java
@@ -8,27 +8,33 @@ import java.util.List;
  **/
 public interface MyFileReaderInterface {
     /**
-     * Method getNumberOfLines returns the number of rows this file has.
+     * Method getNumberOfLines returns the number of rows this file has
+     * 
+     * @return returns the number of rows this file has or -1 on failure.
      *
      **/
     int getNumberOfLines();
 
     /**
-     * Method getNumberOfNonEmptyLines returns number of non-empty rows from the file.
+     * Method getNumberOfNonEmptyLines returns number of non-empty rows from the file
+     * 
+     * @return returns number of non-empty rows from the file or -1 on failure.
      **/
     int getNumberOfNonEmptyLines();
 
     /**
      * Method readLines reads contents of the file an returns lines read.
      * 
-     * @return the lines read
+     * @return the lines read or null on failure
      **/
     List<String> readLines();
 
     /**
      * Method readFirstNLines reads first n lines from the file.
      * 
-     * @return the lines read.
+     * @return the lines read or null on failure
+     * @param numOfLines
+     *            The number of lines to be read.
      **/
     List<String> readFirstNLines(int numOfLines);
 
