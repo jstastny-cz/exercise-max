6524f3ea85c451ba50ddaceb66137a640cdcb27f
mkosiarc <mkosiarc@dhcp-10-40-4-108.brq.redhat.com>
2017-11-09 16:55:11 +0100

    add exception message,add check for exceptions

--- exercise/src/main/java/org/jboss/qe/dvqe/MyFileReader.java
+++ exercise/src/main/java/org/jboss/qe/dvqe/MyFileReader.java
@@ -14,11 +14,11 @@ class MyFileReader implements MyFileReaderInterface {
 
     MyFileReader(String path) throws FileNotFoundException {
         if (path == null || path.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Missing argument");
         }
         file = new File(path);
         if (!file.isFile()) {
-            throw new FileNotFoundException();
+            throw new FileNotFoundException("File doesn't exist");
         }
     }
 
@@ -28,55 +28,58 @@ class MyFileReader implements MyFileReaderInterface {
     }
 
     public int getNumberOfLines() {
-        int numOfLines = -1;
+        int numOfLines;
         try (Scanner sc = getScanner()) {
             numOfLines = 0;
             while (sc.hasNextLine()) {
                 numOfLines++;
                 sc.nextLine();
             }
-        } catch (FileNotFoundException e) {
-            System.err.println("File not Found");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + e.getMessage());
+            numOfLines = -1;
         }
         return numOfLines;
     }
 
     public int getNumberOfNonEmptyLines() {
-        int numOfNonEmptyLines = -1;
+        int numOfNonEmptyLines;
         try (Scanner scanner = getScanner()) {
             numOfNonEmptyLines = 0;
             while (scanner.hasNextLine()) {
                 if (!(scanner.nextLine().isEmpty()))
                     numOfNonEmptyLines++;
             }
-        } catch (FileNotFoundException e) {
-            System.err.println("File not Found");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + e.getMessage());
+            numOfNonEmptyLines = -1;
         }
         return numOfNonEmptyLines;
     }
 
     public List<String> readLines() {
-        List<String> lines = null;
+        List<String> lines;
         try (Scanner scanner = getScanner()) {
             lines = new ArrayList<String>();
             while (scanner.hasNextLine()) {
                 lines.add(scanner.nextLine());
             }
-
-        } catch (FileNotFoundException e) {
-            System.err.println("File not Found");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + e.getMessage());
+            lines = null;
         }
         return lines;
     }
 
     public List<String> readFirstNLines(int numOfLines) {
-        List<String> lines = null;
+        List<String> lines;
         try (Scanner scanner = getScanner()) {
             lines = new ArrayList<String>();
             for (int i = 0; scanner.hasNextLine() && i < numOfLines; i++)
                 lines.add(scanner.nextLine());
-        } catch (FileNotFoundException e) {
-            System.err.println("File not Found");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + e.getMessage());
+            lines = null;
         }
         return lines;
     }
